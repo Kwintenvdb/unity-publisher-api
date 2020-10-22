79451dc94a353f9b64b70aaca87f3894036a83f0
kwintenvdb <kwintenvdberghe@gmail.com>
2020-10-22 22:49:02 +0200

    Extended package data

--- src/api/models/packageData.ts
+++ src/api/models/packageData.ts
@@ -2,6 +2,10 @@ export interface PackageData {
     id: string;
     name: string;
     url: string;
+    averageRating: number;
+    numRatings: number;
+    price: number;
+    version: string;
 }
 
 export function toPackageData(rawData: any): PackageData {
@@ -9,5 +13,9 @@ export function toPackageData(rawData: any): PackageData {
         id: rawData.id,
         name: rawData.name,
         url: rawData.short_url,
+        averageRating: rawData.average_rating,
+        numRatings: rawData.count_ratings,
+        price: rawData.versions[0].price,
+        version: rawData.versions[0].version_name,
     };
 }
